96ddd9da748b53494f25dfa50b517623f1775a25
Saad Khurshid Qurashi <saadkhurshidqureshi@gmail.com>
2025-05-13 11:17:46 +0500

    deleted firebase

--- main.py
+++ main.py
@@ -1,31 +1,19 @@
-# main.py
-
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-
-# Flask-Uploads & SQLAlchemy config
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
-app.config['ALLOWED_EXTENSIONS'] = {'jpg','jpeg','png','gif'}
+app.config['ALLOWED_EXTENSIONS'] = {'jpg', 'jpeg', 'png', 'gif'}
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:pgadmin4@localhost:5432/ecomproject'
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# Firebase backup config
-app.config['FIREBASE_SERVICE_ACCOUNT'] = 'serviceAccountKey.json'
-app.config['FIREBASE_BUCKET']          = 'kuchu-muchu.appspot.com'
 
 db = SQLAlchemy(app)
 
-# register your backup CLI command
-from backup import backup_db_command
-app.cli.add_command(backup_db_command)
-
-# import your views
+# Import views at the end to avoid circular imports
 from views import *
 from admin import *
 
